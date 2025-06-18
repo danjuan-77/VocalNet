73974187df405f754bf121218429330b805fbf7a
danjuan-77 <youngertutu@gmail.com>
2025-06-18 14:24:59 +0800

    update

--- scripts/mtp/llama_stage2_s2s_server.sh
+++ scripts/mtp/llama_stage2_s2s_server.sh
@@ -42,13 +42,13 @@ deepspeed omni_speech/train/train_mem.py \
     --bf16 True \
     --output_dir "$CHECKPOINT_DIR" \
     --num_train_epochs 3 \
-    --per_device_train_batch_size 1 \
-    --per_device_eval_batch_size 1 \
-    --gradient_accumulation_steps 1 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 4 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 500 \
-    --save_total_limit 1 \
+    --save_steps 10000 \
+    --save_total_limit 10 \
     --learning_rate 2e-4 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
