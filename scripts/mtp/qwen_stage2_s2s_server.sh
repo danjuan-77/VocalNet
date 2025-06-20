84c7c51c2612e8d5dc81e2f41da79134628cb711
danjuan-77 <youngertutu@gmail.com>
2025-06-20 12:35:08 +0800

    update

---
+++ scripts/mtp/qwen_stage2_s2s_server.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+export WANDB_ENTITY="kevin-tutu"
+export WANDB_PROJECT="vocalnet-sft"
+export TOKENIZERS_PARALLELISM=false
+
+CHECKPOINT_NAME="qwen25-7b-instruct-s2s-mtp-ultravoice-all-sft"
+CHECKPOINT_DIR="./checkpoints/${CHECKPOINT_NAME}"
+BASE_MODEL="/share/nlp/tuwenming/models/VocalNet/VocalNet-qwen25-7B" 
+DATA_PATH="./playground/ultravoice_all_train_104663.json"
+SPEECH_FOLDER="./playground/"
+SPEECH_ENCODER="/share/nlp/tuwenming/models/openai/whisper-large-v3"
+
+# 创建日志目录
+mkdir -p "$CHECKPOINT_DIR"
+
+# 获取当前日期和时间
+timestamp=$(date +"%Y%m%d_%H%M%S")
+log_file="${CHECKPOINT_DIR}/${CHECKPOINT_NAME}-${timestamp}.log"
+echo "Starting training at $(date)"
+echo "Logging to $log_file"
+
+deepspeed --master_port 29602 --include localhost:0,1,2,3 omni_speech/train/train_mem.py \
+    --lora_enable False \
+    --deepspeed ./scripts/zero3.json \
+    --model_name_or_path $BASE_MODEL \
+    --version qwen_2_5 \
+    --input_type mel \
+    --mel_size 128 \
+    --speech_encoder_type whisper \
+    --speech_projector_type linear \
+    --tune_speech_generator_only True \
+    --has_speech_generator True \
+    --mm_tunable_parts "speech_generator" \
+    --is_multimodal \
+    --data_path $DATA_PATH \
+    --speech_folder $SPEECH_FOLDER \
+    --speech_encoder $SPEECH_ENCODER \
+    --group_by_modality_length True \
+    --bf16 True \
+    --output_dir "$CHECKPOINT_DIR" \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 4 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 1000 \
+    --save_total_limit 10 \
+    --learning_rate 2e-4 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --model_max_length 4096 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 4 \
+    --lazy_preprocess True \
+    --run_name "${CHECKPOINT_NAME}-$(date +"%Y%m%d")" \
+    --report_to wandb &> "$log_file"
+
+
