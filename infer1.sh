5d3db240b51520de9f44eae89e944d6d5424e9fa
danjuan-77 <youngertutu@gmail.com>
2025-06-25 14:21:53 +0800

    update

--- infer1.sh
+++ infer1.sh
@@ -3,3 +3,9 @@ python3 omni_speech/infer/vocalnet_batch_infer.py --model_path /share/nlp/tuwenm
     --test_jsonl /share/nlp/tuwenming/projects/UltraVoice_dev/data/metadata_tiny/test/ultravoice_testset.jsonl \
     --s2s \
     --save_dir ./infer_results/VocalNet-qwen25-7b-SFT-llm-and-decoder-4000/wav
+
+
+python3 omni_speech/infer/vocalnet_batch_infer.py --model_path /share/nlp/tuwenming/projects/VocalNet/checkpoints/llama32-1B-instruct-s2s-mtp-ultravoice-all-sft-llm-and-decoder/checkpoint-4000 \
+    --test_jsonl /share/nlp/tuwenming/projects/UltraVoice_dev/data/metadata_tiny/test/ultravoice_testset.jsonl \
+    --s2s \
+    --save_dir ./infer_results/VocalNet-llama32-1B-SFT-llm-and-decoder-4000/wav
