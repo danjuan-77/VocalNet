c55cd1254b9e015f0f21ac0c98717fc082435640
danjuan-77 <youngertutu@gmail.com>
2025-06-26 15:00:35 +0800

    update

--- infer1.sh
+++ infer1.sh
@@ -1,11 +1,5 @@
 export CUDA_VISIBLE_DEVICES=1
-python3 omni_speech/infer/vocalnet_batch_infer.py --model_path /share/nlp/tuwenming/projects/VocalNet/checkpoints/qwen25-7b-instruct-s2s-mtp-ultravoice-all-sft-llm-and-decoder/checkpoint-4000 \
+python3 omni_speech/infer/vocalnet_batch_infer.py --model_path /share/nlp/tuwenming/projects/VocalNet/checkpoints/qwen25-7b-instruct-s2s-mtp-ultravoice-all-sft/checkpoint-1000 \
     --test_jsonl /share/nlp/tuwenming/projects/UltraVoice_dev/data/metadata_tiny/test/ultravoice_testset.jsonl \
     --s2s \
-    --save_dir ./infer_results/VocalNet-qwen25-7b-SFT-llm-and-decoder-4000/wav
-
-
-python3 omni_speech/infer/vocalnet_batch_infer.py --model_path /share/nlp/tuwenming/projects/VocalNet/checkpoints/llama32-1B-instruct-s2s-mtp-ultravoice-all-sft-llm-and-decoder/checkpoint-4000 \
-    --test_jsonl /share/nlp/tuwenming/projects/UltraVoice_dev/data/metadata_tiny/test/ultravoice_testset.jsonl \
-    --s2s \
-    --save_dir ./infer_results/VocalNet-llama32-1B-SFT-llm-and-decoder-4000/wav
+    --save_dir ./infer_results/VocalNet-qwen25-7b-SFT-1000/wav
--- infer4.sh
+++ infer4.sh
@@ -1,5 +1,5 @@
 export CUDA_VISIBLE_DEVICES=0
-python3 omni_speech/infer/vocalnet_batch_infer.py --model_path /share/nlp/tuwenming/models/VocalNet/VocalNet-qwen25-7B \
+python3 omni_speech/infer/vocalnet_batch_infer.py --model_path /share/nlp/tuwenming/projects/VocalNet/checkpoints/llama32-8B-instruct-s2s-mtp-ultravoice-all-sft-llm-and-decoder/checkpoint-2000 \
     --test_jsonl /share/nlp/tuwenming/projects/UltraVoice_dev/data/metadata_tiny/test/ultravoice_testset.jsonl \
     --s2s \
-    --save_dir ./infer_results/VocalNet-qwen25-7b/wav
+    --save_dir ./infer_results/VocalNet-llama32-8B-SFT-llm-and-decoder-2000/wav
