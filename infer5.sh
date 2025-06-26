47f71d980c8f4c881995fa37b2e49bff5703ad34
danjuan-77 <youngertutu@gmail.com>
2025-06-26 15:08:22 +0800

    update

--- infer5.sh
+++ infer5.sh
@@ -2,4 +2,4 @@ export CUDA_VISIBLE_DEVICES=1
 python3 omni_speech/infer/vocalnet_batch_infer.py --model_path /share/nlp/tuwenming/projects/VocalNet/checkpoints/qwen25-7b-instruct-s2s-mtp-ultravoice-all-sft-llm-and-decoder/checkpoint-3000 \
     --test_jsonl /share/nlp/tuwenming/projects/UltraVoice_dev/data/metadata_tiny/test/ultravoice_testset.jsonl \
     --s2s \
-    --save_dir ./infer_results/VocalNet-qwen25-7b-SFT-llm-and-decoder-3000/wav
+    --save_dir ./infer_results/VocalNet-llama32-8B-SFT-llm-and-decoder-3000/wav
--- infer6.sh
+++ infer6.sh
@@ -2,4 +2,4 @@ export CUDA_VISIBLE_DEVICES=2
 python3 omni_speech/infer/vocalnet_batch_infer.py --model_path /share/nlp/tuwenming/projects/VocalNet/checkpoints/llama32-1B-instruct-s2s-mtp-ultravoice-all-sft/checkpoint-4000 \
     --test_jsonl /share/nlp/tuwenming/projects/UltraVoice_dev/data/metadata_tiny/test/ultravoice_testset.jsonl \
     --s2s \
-    --save_dir ./infer_results/VocalNet-1B-SFT-4000/wav
+    --save_dir ./infer_results/VocalNet-llama32-8B-SFT-llm-and-decoder-4000/wav
