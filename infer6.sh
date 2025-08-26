b2fa19f48166ab3080cf195fe15b785e36095430
danjuan-77 <youngertutu@gmail.com>
2025-08-26 23:21:45 +0800

    update

--- infer6.sh
+++ infer6.sh
@@ -2,4 +2,4 @@ export CUDA_VISIBLE_DEVICES=2
 python3 omni_speech/infer/vocalnet_batch_infer.py --model_path /share/nlp/tuwenming/projects/VocalNet/checkpoints/llama32-8B-instruct-s2s-mtp-ultravoice100k-clean-all-sft-llm-and-decoder-save-steps200/checkpoint-2600 \
     --test_jsonl /share/nlp/tuwenming/projects/UltraVoice_dev/data/metadata_tiny/test/ultravoice_testset.jsonl \
     --s2s \
-    --save_dir ./infer_results/VocalNet-llama32-8B-SFT-llm-and-decoder-2600/wav
+    --save_dir /share/nlp/kangyipeng/infer_results/VocalNet-llama32-8B-SFT-llm-and-decoder-2600/wav
