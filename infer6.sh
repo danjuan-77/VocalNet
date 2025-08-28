2f0717b6c33752437b1af28ce4bec159403520fb
danjuan-77 <youngertutu@gmail.com>
2025-08-28 18:38:23 +0800

    update

--- infer6.sh
+++ infer6.sh
@@ -1,5 +1,5 @@
 export CUDA_VISIBLE_DEVICES=2
-python3 omni_speech/infer/vocalnet_batch_infer.py --model_path /share/nlp/tuwenming/projects/VocalNet/checkpoints/llama32-8B-instruct-s2s-mtp-ultravoice100k-clean-all-sft-llm-and-decoder-save-steps200/checkpoint-1600 \
+python3 omni_speech/infer/vocalnet_batch_infer.py --model_path /mnt/buffer/kangyipeng/checkpoints/VocalNet/llama32-8B-instruct-s2s-mtp-ultravoice100k-clean-all-sft-llm-and-decoder-save-steps200/checkpoint-1600 \
     --test_jsonl /share/nlp/tuwenming/projects/UltraVoice_dev/data/metadata_tiny/test/ultravoice_testset.jsonl \
     --s2s \
     --save_dir /share/nlp/kangyipeng/infer_results/VocalNet-llama32-8B-SFT-llm-and-decoder-ultravoice100k-clean-1600/wav
--- infer7.sh
+++ infer7.sh
@@ -1,5 +1,5 @@
 export CUDA_VISIBLE_DEVICES=3
-python3 omni_speech/infer/vocalnet_batch_infer.py --model_path /share/nlp/tuwenming/projects/VocalNet/checkpoints/llama32-8B-instruct-s2s-mtp-ultravoice100k-clean-all-sft-llm-and-decoder-save-steps200/checkpoint-1800 \
+python3 omni_speech/infer/vocalnet_batch_infer.py --model_path /mnt/buffer/kangyipeng/checkpoints/VocalNet/llama32-8B-instruct-s2s-mtp-ultravoice100k-clean-all-sft-llm-and-decoder-save-steps200/checkpoint-1800 \
     --test_jsonl /share/nlp/tuwenming/projects/UltraVoice_dev/data/metadata_tiny/test/ultravoice_testset.jsonl \
     --s2s \
     --save_dir /share/nlp/kangyipeng/infer_results/VocalNet-llama32-8B-SFT-llm-and-decoder-ultravoice100k-clean-1800/wav
