fee505b8ff9d43e5c3d298daca86818e55219e6f
danjuan-77 <youngertutu@gmail.com>
2025-08-29 17:53:58 +0800

    update

--- infer4.sh
+++ infer4.sh
@@ -1,5 +1,5 @@
 export CUDA_VISIBLE_DEVICES=0
-python3 omni_speech/infer/vocalnet_batch_infer.py --model_path /mnt/buffer/kangyipeng/checkpoints/VocalNet/llama32-8B-instruct-s2s-mtp-ultravoice100k-clean-all-sft-llm-and-decoder-save-steps200/checkpoint-1200 \
+python3 omni_speech/infer/vocalnet_batch_infer.py --model_path /share/nlp/tuwenming/projects/VocalNet/checkpoints/qwen25-7B-instruct-s2s-mtp-ultravoice100k-clean-all-sft-llm-and-decoder-save-steps200/checkpoint-200 \
     --test_jsonl /share/nlp/tuwenming/projects/UltraVoice_dev/data/metadata_tiny/test/ultravoice_testset.jsonl \
     --s2s \
-    --save_dir /share/nlp/kangyipeng/infer_results/VocalNet-llama32-8B-SFT-llm-and-decoder-ultravoice100k-clean-1200/wav
+    --save_dir /share/nlp/kangyipeng/infer_results/VocalNet-qwen25-7B-SFT-llm-and-decoder-ultravoice100k-clean-200/wav
